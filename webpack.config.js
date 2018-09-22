3825767928b02e58feaab48e788866687e379afc
李桂炎 <partrick.li@gmail.com>
2018-09-22 15:45:57 +0800

    refactor: Delete global variable, which not work correctly with webpack
    Global is undefined after source code compiled by webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   mode: 'development',
   entry: {
-    app: './js/app.js',
+    engine: './js/engine.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
